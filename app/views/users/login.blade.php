d68a8d56ffa565b3f45ec87b6a95cfadc5d44e81
maitho <thigedavidmaitho@gmail.com>
2014-10-08 14:40:49 +0300

    my second commit

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -1,5 +1,5 @@
 {{ Form::open(array('url'=>'users/signin', 'class'=>'form-signin')) }}
-	<h2 class="form-signin-heading">Please Login</h2>
+	<h2 class="form-signin-heading">Please Login here</h2>
 
 	{{ Form::text('email', null, array('class'=>'input-block-level', 'placeholder'=>'Email Address')) }}
 	{{ Form::password('password', array('class'=>'input-block-level', 'placeholder'=>'Password')) }}
